6c64ca3ebe38847ea5e16085bbbda6e049fd731a
Nick Mowen <nickmowen213@gmail.com>
2018-12-09 17:35:05 -0700

    Bug Fixes

--- app/src/main/java/com/nick/mowen/bellmanfords/data/KotlinMain.kt
+++ app/src/main/java/com/nick/mowen/bellmanfords/data/KotlinMain.kt
@@ -1,8 +1,5 @@
 package com.nick.mowen.bellmanfords.data
 
-import java.util.*
-import kotlin.collections.ArrayList
-
 object KotlinMain {
 
     fun main() {
@@ -28,53 +25,4 @@ object KotlinMain {
             require(it[4].cost == 1)
         }
     }
-
-    fun main(args: Array<String>) {
-
-        fun printMenu() {
-            print(
-                """
-                    1: Add Edge
-                    2: Run Bellman Fords
-                    3: Time Test
-                   4+: Quit
-                   Choice:
-                """.trimIndent()
-            )
-        }
-
-        val `in` = Scanner(System.`in`)
-        printMenu()
-        var choice = `in`.nextLine().toIntOrNull() ?: 4
-        val list = ArrayList<Graph.Edge>()
-
-        do {
-            when (choice) {
-                1 -> {
-                    print("Enter Start End Weight: ")
-                    list.add(Graph.Edge(`in`.nextInt(), `in`.nextInt(), `in`.nextInt()))
-                }
-                2 -> {
-                    print("Enter source vertex: ")
-                    println("\nResults: ")
-                    ShortestPath.findShortestPath(Graph(list.getVertexCount(), list.size, list.toTypedArray()), `in`.nextInt()).forEach {
-                        print("Distance: ${it.cost} Link: ${it.name}")
-                    }
-                }
-                3 -> {
-                    print("Enter source vertex: ")
-                    val index = `in`.nextInt()
-                    val start = System.currentTimeMillis()
-                    ShortestPath.findShortestPath(Graph(list.getVertexCount(), list.size, list.toTypedArray()), index)
-                    println("Took ${start - System.currentTimeMillis()} milliseconds")
-                }
-            }
-
-            printMenu()
-            choice = `in`.nextInt()
-        } while (choice < 4)
-    }
-
-    private fun ArrayList<Graph.Edge>.getVertexCount() =
-        (this.map { it.source } + this.map { it.destination }).distinct().size
 }
